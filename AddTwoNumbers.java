287d86c534b7f42f6abf99ac32941258969ed463
srinivasch0306 <srinivasch2202@gmail.com>
2024-06-21 21:12:05 +0530

    adding tow numbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
         int num1 = scanner.nextInt();
